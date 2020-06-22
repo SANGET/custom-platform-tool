c033dd53bc782f885e8d103861dd95315ae7c188
sanget <zh.sanget@gmail.com>
2020-06-22 16:16:08 +0800

    Fix consumer's name

--- packages/provider-app-hub/EntryHall/src/app.tsx
+++ packages/provider-app-hub/EntryHall/src/app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from "react-router-dom";
 
 import DataManager from "@provider-app/data-manager/app";
 import PageManager from "@provider-app/page-manager/app";
