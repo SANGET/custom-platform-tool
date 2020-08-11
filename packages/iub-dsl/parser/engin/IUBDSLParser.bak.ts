8ef525177d0f825dd3737b2439cf625f9fff703f
Kocher-JGC <879515820@qq.com>
2020-08-11 11:47:42 +0800

    feat(IUB-DSL): Bak Old Case codePart

--- packages/iub-dsl/parser/engin/IUBDSLParser.bak.ts
+++ packages/iub-dsl/parser/engin/IUBDSLParser.bak.ts
@@ -90,6 +90,82 @@ const IUBDSLParser = ({
 //     parsesysOutput();
 //   }
 
+// 绑定数据、观察变化、局部优化
+// TODO: 每次都是重新解析渲染
+/**
+ * 预编译
+ * 实际使用
+ * dataChange
+ * vaild --> fn/view
+ * 由规则进行绑定事件~~~
+ * 流程控制
+ * context?
+ * 数据变更关系的解析、数据收集
+ * 弹窗、页面通信
+ * 校验: TODO
+ *
+ * Relation: 解决副作用、拦截、通用、动态插拔的关系??
+ */
+const IUBDSLParser2 = ({
+  dsl, context // 需要的上下文依赖
+}: ParserParamsOfIUBDSL) => {
+  const {
+    layoutContent, componentsCollection, actionsCollection,
+    metadataCollection, relationshipsCollection, schemas,
+    sysRtCxtInterface
+  } = dsl;
+  const d = Date.now();
+  let parseRelationRes: CommonObjStruct = {};
+  let parseContext = {};
+  // 数据模型解析
+  const {
+    // pageRuntimeState, setPageRuntimeState
+    schemaStruct, mappingEntity
+  } = DataSchemasParser({ metadataCollection, schemas });
+
+  // TODO: 建立引用关系,动态获取其他页面引用的数据 ??
+  const { pageRuntimeState, setPageRuntimeState } = InitPageState(schemaStruct);
+  parseContext = {
+    mappingEntity,
+    pageRuntimeState,
+    setPageRuntimeState,
+  };
+  // action的依赖??
+  parseRelationRes = RelationParser('DataSchemasParseEnd', relationshipsCollection, parseContext);
+  console.log(parseRelationRes);
+
+  const parseActionResult = ActionsCollectionParser(actionsCollection);
+
+  parseContext = {
+    ...parseContext,
+    ...parseRelationRes,
+    bindAction: (actionID) => parseActionResult[actionID]
+  };
+
+  // 验证依赖和时机是否满足、满足则执行
+  // parseRelation('', relationshipsCollection, parseContext);
+
+  const componentParseRes = ComponentCollectionParser(componentsCollection, parseContext);
+
+  parseContext = {
+    ...parseContext,
+    bindComponent: (compId) => componentParseRes[compId]
+  };
+
+  const layoutParseRes = LayoutParser({ layoutNode: layoutContent.content }, parseContext);
+
+  parseContext = {
+    ...parseContext,
+    layoutParseRes,
+  };
+
+  console.log(Date.now() - d);
+
+  console.log(parseContext);
+
+  return parseContext;
+};
+
 export default IUBDSLParser;
 
 // 行为与表现分离
