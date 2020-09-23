029b93a170c5c9ebd61a04b14912bde0b202aa86
Kocher-JGC <879515820@qq.com>
2020-09-23 20:54:22 +0800

    update: 修改错误文件

--- packages/engine/iub-dsl/engine/component-manage/component-parser/base-input-parser.ts
+++ packages/engine/iub-dsl/engine/component-manage/component-parser/base-input-parser.ts
@@ -1,7 +1,7 @@
 import { baseInputCompParser as defaultInputParser } from "../component-store/form/input/base-input-parser";
 import { globalInputConf, globalDefaultOptions } from "../../conf";
 
-export const baseInputCompParser = (id, conf, options = {}) => {
+export const baseInputCompParser = (id: string, conf = {}, options = {}) => {
   return defaultInputParser(id, {
     ...globalInputConf,
     ...conf
