789810430a927dde8bd62860f50a50342997ade2
hdl <1158025556@qq.com>
2020-09-23 16:38:33 +0800

    feat: 新增预览模块

--- packages/web-platform/config/router.ts
+++ packages/web-platform/config/router.ts
@@ -22,6 +22,9 @@ export default [{
         },{
           path: '/page',
           component: './Welcome',
+        },{
+          path: '/preview',
+          component: './Welcome',
         },
         {
           path: '/dashboard',
