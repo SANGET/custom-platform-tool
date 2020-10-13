5ab9ad5c26efecb35c8af75d589463d88d2f5c96
lijia <lijia@haoyuntech.com>
2020-10-13 13:56:59 +0800

    feat: 增加中文支持

--- packages/provider-app-hub/TableEditor/app.tsx
+++ packages/provider-app-hub/TableEditor/app.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
 import TableEditor from "./components/TableEditor";
 import "./index.less";
 
 const App: HY.SubApp = (props) => {
   return (
-    <div className="page-list-container">
-      <TableEditor />
-    </div>
+
+    <ConfigProvider locale={zhCN}>
+      <div className="page-list-container">
+        <TableEditor />
+      </div>
+    </ConfigProvider>
   );
 };
 
