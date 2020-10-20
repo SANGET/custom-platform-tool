43e615e70aab01bffa77db520331d28ddbed2002
zxj <zhangxiangjie@haoyuntech.com>
2020-10-20 21:09:52 +0800

    feat: 调整工程的配置文件

--- packages/provider-app-common/config/getPreviewUrl.ts
+++ packages/provider-app-common/config/getPreviewUrl.ts
@@ -1,8 +1,8 @@
-const baseUrl = 'http://localhost:8000';
-// const baseUrl = 'http://10.11.6.193:8000';
+import { getAppConfig } from "./config-manager";
 
 export const getPreviewUrl = (appLocation?, appName = '测试应用') => {
-  if (!appLocation) return `${baseUrl}`;
+  const perviewAppUrl = getAppConfig('perviewAppUrl');
+  if (!appLocation) return `${perviewAppUrl}`;
   const { pageID, lessee = 'hy', app } = appLocation;
-  return `${baseUrl}/page?menuid=/preview&mode=preview&pageId=${pageID}&lessee=${lessee}&app=${app}&appName=${appName}`;
+  return `${perviewAppUrl}/page?menuid=/preview&mode=preview&pageId=${pageID}&lessee=${lessee}&app=${app}&appName=${appName}`;
 };
