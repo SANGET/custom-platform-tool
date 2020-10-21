27413638d62353327b941bbd25755b81b43c0547
lijia <lijia@haoyuntech.com>
2020-10-21 14:12:23 +0800

    feat: 增加图标弹窗底部按钮

--- packages/provider-app-hub/MenuManager/pages/SelectIcon.tsx
+++ packages/provider-app-hub/MenuManager/pages/SelectIcon.tsx
@@ -18,6 +18,7 @@ import * as AllIconsRI from "react-icons/ri";
 import * as AllIconsWI from "react-icons/wi";
 import * as allIcons from "react-icons/all";
 import { Tabs } from 'antd';
+import { ModalFooter } from '@provider-app/table-editor/components/ChooseDict';
 
 const allIconsByArea = {
   AllIconsAI,
@@ -51,11 +52,20 @@ const getIconList = (listAreaKey) => {
   const IconItems:any = [];
   const listArea = allIconsByArea[listAreaKey];
   Object.keys(listArea).map((key) => {
-    IconItems.push(listArea[key]);
+    IconItems.push({ type: key, icon: listArea[key] });
   });
   return IconItems;
 };
 const SelectIcon: React.FC<ISelectPage> = (props: ISelectPage) => {
+  const {
+    currentIcon, onOk, onCancel
+  } = props;
+  const [typeInState, setIconInState] = useState<string>('');
+  useEffect(() => {
+    return () => {
+      setIconInState(currentIcon);
+    };
+  }, [currentIcon]);
   const list = [
     { key: 'AllIconsAI', tabName: '电脑图标' },
     { key: 'AllIconsBI', tabName: '电脑图标' },
@@ -75,26 +85,53 @@ const SelectIcon: React.FC<ISelectPage> = (props: ISelectPage) => {
     { key: 'AllIconsRI', tabName: '电脑图标' },
     { key: 'AllIconsWI', tabName: '电脑图标' }
   ];
+  const handleOk = () => {
+    onOk && onOk(page);
+  };
+  const handleCancel = () => {
+    onCancel && onCancel();
+  };
+  const getClassName = (typeLoop) => {
+    if (iconInState === typeLoop) {
+      return "float-left text-xl p-2 m-1 text-blue-500";
+    }
+    return "float-left text-xl p-2 m-1";
+  };
+  const handleClick = () => {
+
+  };
   return (
-    <Tabs defaultActiveKey="1">
-      {
-        list.map((item, index) => {
-          return (
-            <TabPane tab={item.tabName + index} key={item.key} className="overflow-auto" style={{ height: 300 }}>
-              {
-                getIconList(item.key).map((IconItem) => {
-                  return (
-                    <div className="float-left text-xl p-2 m-1" key = {IconItem}>
-                      <IconItem/>
-                    </div>
-                  );
-                })
-              }
-            </TabPane>
-          );
-        })
-      }
-    </Tabs>
+    <>
+      <Tabs defaultActiveKey="1" style={{ marginTop: -15 }}>
+        {
+          list.map((item, index) => {
+            return (
+              <TabPane tab={item.tabName + index} key={item.key} className="overflow-auto mb-2" style={{ height: 300 }}>
+                {
+                  getIconList(item.key).map(({ type, icon: IconItem }) => {
+                    return (
+                      <div
+                        className={getClassName(type)}
+                        key = {type}
+                        onClick={() => {
+                          setIconInState(type);
+                        }}
+                      >
+                        <IconItem/>
+                      </div>
+                    );
+                  })
+                }
+              </TabPane>
+            );
+          })
+        }
+      </Tabs>
+      <ModalFooter
+        onCancel={handleCancel}
+        onOk={handleOk}
+      />
+    </>
   );
 };
 
