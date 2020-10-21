96645fd2eeef12e6b1737562f49579dc336a91fa
zxj <zhangxiangjie@haoyuntech.com>
2020-10-21 15:13:11 +0800

    fix: 修复多个 react dom 导致的问题

--- packages/engine/visual-editor/core/visual-app-connector.tsx
+++ packages/engine/visual-editor/core/visual-app-connector.tsx
@@ -67,13 +67,13 @@ const Connector = (
   if (!appKey) {
     throw Error('appKey 是必须且唯一的，请检查调用');
   }
-  let C = ConnectedAppStore[appKey];
-  if (!C) {
+  let ConnectorChild = ConnectedAppStore[appKey];
+  if (!ConnectorChild) {
     const appStore = createStore(appKey);
     // console.log(object);
     const connector = connect(mapVisualStateToProps, mapVisualDispatchToProps(appKey));
     const Comp = connector(ConnectApp);
-    C = (props) => {
+    ConnectorChild = (props) => {
       return (
         <DndProvider backend={HTML5Backend}>
           <ReduxProvider store={appStore}>
@@ -82,9 +82,9 @@ const Connector = (
         </DndProvider>
       );
     };
-    ConnectedAppStore[appKey] = C;
+    ConnectedAppStore[appKey] = ConnectorChild;
   }
-  return C;
+  return ConnectorChild;
 };
 
 export default Connector;
