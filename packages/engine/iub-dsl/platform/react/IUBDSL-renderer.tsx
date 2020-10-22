1c34a1159dcb07e9ec219c5b9f34ee3838d8e3bf
Kocher-JGC <879515820@qq.com>
2020-10-22 19:36:13 +0800

    update: 添加注释完善代码

--- packages/engine/iub-dsl/platform/react/IUBDSL-renderer.tsx
+++ packages/engine/iub-dsl/platform/react/IUBDSL-renderer.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 import { IUBDSLParser, IUBDSLRuntimeContainer } from '@iub-dsl/engine';
-// import { testDemo } from '@iub-dsl/demo/base-reference';
-import { userDemo } from '@iub-dsl/demo/base-reference/user/user';
 
 const resolvedDsl: {
   [pageId: string]: any; // IUB_DSL解析结果
@@ -13,36 +11,35 @@ const resolvedDsl: {
 const IUBDSLRenderer = React.memo<{dsl: any}>(({ dsl }) => {
   const { pageID } = dsl;
   if (typeof pageID !== 'string') {
-    dsl = userDemo;
-    // return <ErrorRenderer msg='IUB-DSL Data Error'/>;
+    return <ErrorRenderer msg='IUB-DSL Data Error'/>;
   }
-  // 临时添加schemas
-  // dsl.schemas = testSchemas;
 
   let dslParseRes;
+  let ActualRender = <ErrorRenderer/>;
   try {
     if (!(dslParseRes = resolvedDsl[pageID])) {
       dslParseRes = IUBDSLParser({ dsl });
+      /** 现在先不缓存解析结果 */
       // resolvedDsl[dsl.pageID] = dslParseRes;
     }
   } catch (e) {
     console.error(e);
-    return <ErrorRenderer msg='IUB-DSLEngine Parser Error?'/>;
+    ActualRender = (<ErrorRenderer msg='IUB-DSLEngine Parser Error?'/>);
   }
 
   if (dslParseRes) {
-    return <IUBDSLRuntimeContainer dslParseRes={dslParseRes} />;
+    ActualRender = <IUBDSLRuntimeContainer dslParseRes={dslParseRes} />;
   }
-  return <ErrorRenderer/>;
+
+  return ActualRender;
 },
-() => {
-  // TODO: 记得删除
-  if (process.env.NODE_ENV === 'development') {
-    return true;
-  }
-  return false;
-}
-// eslint-disable-next-line function-paren-newline
+// () => {
+  // // TODO: 记得删除
+  // if (process.env.NODE_ENV === 'development') {
+  //   return true;
+  // }
+  // return false;
+// }
 );
 
 const ErrorRenderer = ({ msg = '未知' }) => (<div>渲染错误; 错误信息: {msg}</div>);
