750311003433c7fa0daddfdafd66c88e7e08096b
lijia <lijia@haoyuntech.com>
2020-10-24 20:56:10 +0800

    fix: 修复字典管理bug

--- packages/provider-app-hub/DictionaryManager/app.tsx
+++ packages/provider-app-hub/DictionaryManager/app.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN'; // 引入中文包
 import DictionaryList from "./components/DictionaryList";
+
 import "./index.less";
 
 const App: HY.SubApp = (props) => {
   return (
-    <div className="page-list-container">
-      <DictionaryList />
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div className="page-list-container">
+        <DictionaryList />
+      </div>
+    </ConfigProvider>
   );
 };
 
