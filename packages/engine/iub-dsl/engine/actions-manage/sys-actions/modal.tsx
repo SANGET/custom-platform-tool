bb8d38b4d592ba82ad918a773c8d17aa174a2520
Kocher-JGC <879515820@qq.com>
2020-10-26 20:36:24 +0800

    feat: 实现模态框获取页面

--- packages/engine/iub-dsl/engine/actions-manage/sys-actions/modal.tsx
+++ packages/engine/iub-dsl/engine/actions-manage/sys-actions/modal.tsx
@@ -2,6 +2,7 @@ import { Modal, Form, Input } from 'antd';
 import React from 'react';
 import { OpenModal } from '@iub-dsl/definition';
 import { IUBDSLRenderer } from '@iub-dsl/platform/react';
+import { queryPageData } from '@consumer-app/web-platform/src/services/page';
 
 enum ModalType {
   info = 'info',
@@ -21,13 +22,25 @@ export const openModal = (conf: OpenModal) => {
     actionName, actionOutput, actionId
   } = conf;
   return async ({ action, asyncRuntimeScheduler }) => {
-    const { userForm } = await import('@iub-dsl/demo/base-reference/user/userfrom');
-    console.log(userForm);
-
-    Modal.confirm({
-      icon: false,
-      content: <IUBDSLRenderer dsl={userForm} />
-    });
+    if (pageUrl) {
+      try {
+        const pageData = await queryPageData({ id: pageUrl });
+        Modal.confirm({
+          icon: false,
+          content: <IUBDSLRenderer dsl={pageData} />
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      console.error('无pageUrl');
+    }
+    // const { userForm } = await import('@iub-dsl/demo/base-reference/user/userfrom');
+    // console.log(userForm);
+    // Modal.confirm({
+    //   icon: false,
+    //   content: <IUBDSLRenderer dsl={userForm} />
+    // });
     // Modal[ModalType.confirm]({
     //   title: '测试弹窗',
     //   content: <div>你猜猜</div>
